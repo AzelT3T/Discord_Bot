e516315356bc2d8b37825128ddc76d202986f5f6
Mizuno Kosei <84858257+AzelT3T@users.noreply.github.com>
2023-06-25 01:13:50 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,12 @@ import traceback
 from discord.ext import commands
 from os import getenv
 import openai
+from nltk.corpus import wordnet as wn
+from nltk.tokenize import word_tokenize
+import nltk
+
+nltk.download('punkt')
+nltk.download('wordnet')
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -22,10 +28,37 @@ messages = [
     {"role": "assistant", "content": "私はヘルプデスクです。なにかお手伝いできることはありますか？"}
 ]
 
+def word_similarity(word1, word2):
+    word1 = wn.synsets(word1)
+    word2 = wn.synsets(word2)
+    if word1 and word2:  # if word1 and word2 are not empty
+        return word1[0].wup_similarity(word2[0])
+    else:
+        return None
+
+def sentence_similarity(sentence1, sentence2):
+    words1 = word_tokenize(sentence1.lower())
+    words2 = word_tokenize(sentence2.lower())
+    score = 0
+    count = 0
+    for word1 in words1:
+        max_score = 0
+        for word2 in words2:
+            sim = word_similarity(word1, word2)
+            if sim is not None and sim > max_score:
+                max_score = sim
+        if max_score is not None:
+            score += max_score
+            count += 1
+    if count != 0:  # Prevent division by zero
+        return score / count
+    else:
+        return None
+
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
+    error_msg = ''.join(traceback.TracebackException.from_exceptionorig_error).format())
     await ctx.send(error_msg)
 
 @bot.event
@@ -38,10 +71,15 @@ async def on_message(message):
         print(user_message)
         messages.append({"role": "user", "content": user_message})
 
-        # 新たに追加された部分: ユーザーのメッセージがFAQに一致するかどうかをチェック
-        if user_message in faq:
-            response = faq[user_message]
-        else:
+        response = None
+        max_sim = 0.8  # similarity threshold
+        for question in faq:
+            sim = sentence_similarity(user_message, question)
+            if sim is not None and sim > max_sim:
+                response = faq[question]
+                max_sim = sim
+
+        if response is None:
             openai_api_key = getenv('OPENAI_API_KEY')
             openai.api_key = openai_api_key
 
