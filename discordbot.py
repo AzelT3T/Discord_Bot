0137db27286cc7af92b0c9e8c404bc3904ac8484
Mizuno Kosei <84858257+AzelT3T@users.noreply.github.com>
2023-06-25 01:32:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,6 +3,8 @@ import traceback
 from discord.ext import commands
 from os import getenv
 import openai
+from sentence_transformers import SentenceTransformer
+import torch
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -16,11 +18,8 @@ faq = {
     # その他のFAQをここに追加できます
 }
 
-messages = [
-    {"role": "system", "content": "You are a helpful assistant. The AI assistant's name is AI Qiitan."},
-    {"role": "user", "content": "こんにちは。あなたは誰ですか？"},
-    {"role": "assistant", "content": "私はヘルプデスクです。なにかお手伝いできることはありますか？"}
-]
+# Embedding modelの初期化
+model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -36,24 +35,21 @@ async def on_message(message):
         print(message.content)
         user_message = message.content.split('>')[1].lstrip()
         print(user_message)
-        messages.append({"role": "user", "content": user_message})
 
-        # 新たに追加された部分: ユーザーのメッセージがFAQに一致するかどうかをチェック
-        if user_message in faq:
-            response = faq[user_message]
-        else:
-            openai_api_key = getenv('OPENAI_API_KEY')
-            openai.api_key = openai_api_key
+        openai_api_key = getenv('OPENAI_API_KEY')
+        openai.api_key = openai_api_key
 
-            completion = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=messages
-            )
+        # ユーザーのメッセージとFAQの質問の類似度を計算
+        question_embeddings = model.encode(list(faq.keys()))
+        message_embedding = model.encode([user_message])
+        similarities = torch.nn.functional.cosine_similarity(torch.Tensor(message_embedding), torch.Tensor(question_embeddings), dim=-1)
+        closest_idx = similarities.argmax().item()
 
-            response = completion.choices[0].message.content
-
-        print(response)
+        # 最も類似しているFAQの質問を見つけ、対応する回答を取得
+        closest_question = list(faq.keys())[closest_idx]
+        response = faq[closest_question]
+        
+        # メッセージを送信
         await message.channel.send(response)
 
-token = getenv('DISCORD_BOT_TOKEN')
-bot.run(token)
+bot.run(getenv('TOKEN'))
