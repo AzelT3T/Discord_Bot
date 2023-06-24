92e2e46b4496aeb76be184289dcf8f3cd28070d8
Mizuno Kosei <84858257+AzelT3T@users.noreply.github.com>
2023-06-25 01:35:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,23 +3,36 @@ import traceback
 from discord.ext import commands
 from os import getenv
 import openai
-from sentence_transformers import SentenceTransformer
-import torch
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='/', intents=intents)
 
-# ここにFAQを定義します
 faq = {
     "こんにちは。あなたは誰ですか？": "私はヘルプデスクです。なにかお手伝いできることはありますか？",
     "どういう機能がありますか？": "私は質問に答えたり、情報を提供したりします。具体的な要求があればどういった事でもお手伝いできるかもしれません。",
-    # その他のFAQをここに追加できます
 }
 
-# Embedding modelの初期化
-model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+messages = [
+    {"role": "system", "content": "You are a helpful assistant. The AI assistant's name is AI Qiitan."},
+    {"role": "user", "content": "こんにちは。あなたは誰ですか？"},
+    {"role": "assistant", "content": "私はヘルプデスクです。なにかお手伝いできることはありますか？"}
+]
+
+def check_with_openai(question):
+    openai_api_key = getenv('OPENAI_API_KEY')
+    openai.api_key = openai_api_key
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "Does this question: '{}' match with any questions in the FAQ?".format(question)}
+        ]
+    )
+    response = completion.choices[0].message.content
+    return response
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -35,21 +48,22 @@ async def on_message(message):
         print(message.content)
         user_message = message.content.split('>')[1].lstrip()
         print(user_message)
+        messages.append({"role": "user", "content": user_message})
 
-        openai_api_key = getenv('OPENAI_API_KEY')
-        openai.api_key = openai_api_key
+        # If the user message is in the FAQ, return the answer
+        if user_message in faq:
+            response = faq[user_message]
+        else:
+            # If not, check with OpenAI if the question matches any in the FAQ
+            openai_response = check_with_openai(user_message)
+            if "yes" in openai_response.lower():
+                matched_question = openai_response.split(":")[-1].strip()  # Get the matched question from OpenAI's response
+                response = faq[matched_question]
+            else:
+                response = "Sorry, I didn't understand your question. Could you rephrase it or ask something else?"
 
-        # ユーザーのメッセージとFAQの質問の類似度を計算
-        question_embeddings = model.encode(list(faq.keys()))
-        message_embedding = model.encode([user_message])
-        similarities = torch.nn.functional.cosine_similarity(torch.Tensor(message_embedding), torch.Tensor(question_embeddings), dim=-1)
-        closest_idx = similarities.argmax().item()
-
-        # 最も類似しているFAQの質問を見つけ、対応する回答を取得
-        closest_question = list(faq.keys())[closest_idx]
-        response = faq[closest_question]
-        
-        # メッセージを送信
+        print(response)
         await message.channel.send(response)
 
-bot.run(getenv('TOKEN'))
+token = getenv('DISCORD_BOT_TOKEN')
+bot.run(token)
