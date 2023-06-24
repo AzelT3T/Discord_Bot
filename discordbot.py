f35845e7df696593cb65168442d65cedcae08efe
Mizuno Kosei <84858257+AzelT3T@users.noreply.github.com>
2023-06-25 01:51:44 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,21 +14,18 @@ faq = {
     "どういう機能がありますか？": "私は質問に答えたり、情報を提供したりします。具体的な要求があればどういった事でもお手伝いできるかもしれません。",
 }
 
-messages = [
-    {"role": "system", "content": "You are a helpful assistant. The AI assistant's name is AI Qiitan."},
-    {"role": "user", "content": "こんにちは。あなたは誰ですか？"},
-    {"role": "assistant", "content": "私はヘルプデスクです。なにかお手伝いできることはありますか？"}
-]
-
 def check_with_openai(question):
     openai_api_key = getenv('OPENAI_API_KEY')
     openai.api_key = openai_api_key
+    question_list = ", ".join(faq.keys())
+    print(question_list)
+    prompt = f"{question}という質問がありましたが、これは {question_list} のいずれかの質問と一致しますか？回答は"'YES OR NO','一致している質問','一致している質問に対応する回答'"のプロンプトにしたがってください。"
 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": "Does this question: '{}' match with any questions in the FAQ?".format(question)}
+            {"role": "user", "content": prompt}
         ]
     )
     response = completion.choices[0].message.content
@@ -45,24 +42,19 @@ async def on_message(message):
     if message.author == bot.user:
         return
     if bot.user.id in [member.id for member in message.mentions]:
-        print(message.content)
         user_message = message.content.split('>')[1].lstrip()
-        print(user_message)
-        messages.append({"role": "user", "content": user_message})
 
-        # If the user message is in the FAQ, return the answer
-        if user_message in faq:
-            response = faq[user_message]
-        else:
-            # If not, check with OpenAI if the question matches any in the FAQ
-            openai_response = check_with_openai(user_message)
-            if "yes" in openai_response.lower():
-                matched_question = openai_response.split(":")[-1].strip()  # Get the matched question from OpenAI's response
+        # ユーザーのメッセージがFAQに一致するかどうかをチェック
+        response = check_with_openai(user_message)
+        
+        # gpt-3.5-turboの回答が"yes"を含むならば、それに関連するFAQの回答を返す
+        if "yes" in response.lower():
+            matched_question = response.split(",")[1].strip()  # Assuming the second part of the response is the matching question
+            if matched_question in faq:
                 response = faq[matched_question]
-            else:
-                response = "Sorry, I didn't understand your question. Could you rephrase it or ask something else?"
-
-        print(response)
+        else:
+            response = "Sorry, I didn't understand your question. Could you rephrase it or ask something else?"
+        
         await message.channel.send(response)
 
 token = getenv('DISCORD_BOT_TOKEN')
